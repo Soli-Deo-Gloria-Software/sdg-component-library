cb45c1d7ef0f721bb3a3c7fe55dbbc7f258ccc00
Thomas Wood <thomas.matthew.wood@outlook.com>
2026-05-25 15:51:18 -0500

    Deleted ai version.

--- src/components/bible-reference-picker/test/bible-reference-picker.spec.tsx
+++ src/components/bible-reference-picker/test/bible-reference-picker.spec.tsx
@@ -1,29 +1,16 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { BibleReferencePicker } from '../bible-reference-picker';
 
-describe('bible-reference-picker', () => {
+describe('bible-reference-picker-v2', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [BibleReferencePicker],
       html: `<bible-reference-picker></bible-reference-picker>`,
     });
     expect(page.root).toEqualHtml(`
-      <bible-reference-picker max-references="100">
+      <bible-reference-picker>
         <mock:shadow-root>
-          <div class="search-box">
-            <div class="reference-box"></div>
-            <div class="row">
-              <input autocomplete="off" id="input" name="input" placeholder="Scripture Reference" type="text" value="">
-            </div>
-            <div class="result-box">
-              <ul>
-                <li class="listheader">Select Book</li>
-              </ul>
-            </div>
-            <div class="result-box">
-              <ul></ul>
-            </div>
-          </div>
+          <slot></slot>
         </mock:shadow-root>
       </bible-reference-picker>
     `);
