d77e7da6a06bdc2fde9583e29cbcb49f01632f1c
Thomas Wood <thomas.matthew.wood@outlook.com>
2026-09-07 16:50:20 -0500

    updated angular version and bumped package version

--- packages/angular-workspace/projects/test-app/src/app/app.ts
+++ packages/angular-workspace/projects/test-app/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
 })
 export class App {
